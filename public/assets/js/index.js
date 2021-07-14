1e7926d4dc96531f14558f2b594e444594777ded
Zacharyjg <guldb016@umn.edu>
2021-07-14 09:04:16 -0500

    Added change to the deleteNotes function given by prof

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -42,12 +42,21 @@ const saveNote = (note) =>
     body: JSON.stringify(note),
   });
 
-const deleteNote = (id) =>
-  fetch(`/api/notes/${id}`, {
+// const deleteNote = (id) =>
+//   fetch(`/api/notes/${id}`, {
+//     method: 'DELETE',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
+
+const deleteNote = (id = null) =>
+  fetch("/api/notes", {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
     },
+    body: JSON.stringify({id:id})
   });
 
 const renderActiveNote = () => {
