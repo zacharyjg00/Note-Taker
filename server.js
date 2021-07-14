51955751288280ab932cc9fe711b4b864c0cf810
Zacharyjg <guldb016@umn.edu>
2021-07-13 23:46:24 -0500

    Completed GET for /api/notes route; Now grabs data from db.json and populated notes.html

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
@@ -18,7 +19,20 @@ app.get("/notes", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
-    // Do json stuff here
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        } else {
+            return res.json(JSON.parse(data));
+        }
+    })
+});
+
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+
+
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
