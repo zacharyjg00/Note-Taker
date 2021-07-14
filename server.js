0b1602ad0e76e06f14b58ff2aa161cc659cd74d4
Zacharyjg <guldb016@umn.edu>
2021-07-14 09:03:06 -0500

    Completed both the post and delete for the /api/notes routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const express = require("express");
 const fs = require("fs");
+let jsonData = require("./db/db.json");
+let uuid = require("uuid");
 
 const app = express();
 const PORT = 3000;
@@ -19,20 +21,40 @@ app.get("/notes", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    return res.json(jsonData);
+});
+
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    newNote.id = uuid.v4();
+    jsonData.push(newNote);
+    
+    fs.writeFile('./db/db.json', JSON.stringify(jsonData), err => {
         if (err) {
-            console.error(err);
-            return;
+            console.log('Error writing file', err);
         } else {
-            return res.json(JSON.parse(data));
+            return res.json(jsonData);
         }
     })
 });
 
-app.post("/api/notes", (req, res) => {
-    const newNote = req.body;
+app.delete("/api/notes", (req, res) => {
+    let newJson = [];
 
+    jsonData.forEach(note => {
+        if(note.id != req.body.id) {
+            newJson.push(note);
+        }
+    });
 
+    fs.writeFile('./db/db.json', JSON.stringify(newJson), err => {
+        if (err) {
+            console.log('Error writing file', err);
+        } else {
+            jsonData = newJson;
+            return res.json(newJson);
+        }
+    })
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
