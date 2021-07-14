8c1a8671f443cbf7c3711e56872bfada45e2f713
Zacharyjg <guldb016@umn.edu>
2021-07-14 09:40:21 -0500

    Commented code

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+// All of the required packages for the app
 const path = require("path");
 const express = require("express");
 const fs = require("fs");
-let jsonData = require("./db/db.json");
 let uuid = require("uuid");
 
+// This is where the read happens for the db.json file and it is updated throughout the app
+let jsonData = require("./db/db.json");
+
+// Instantiates the express server and the port which it runs on
 const app = express();
 const PORT = 3000;
 
@@ -12,23 +16,27 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+// This is the basic route which will send users to the starting page
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// This is a GET to display the actual notes page
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+// This GET grabs all of the data from the db.json file and returns it to be parsed and displayed by the front end
 app.get("/api/notes", (req, res) => {
     return res.json(jsonData);
 });
 
+// This POST is called whenever the user wishes to save a new note. This generates a unique id for the note and then writes the new set of data to the db.json file
 app.post("/api/notes", (req, res) => {
     let newNote = req.body;
     newNote.id = uuid.v4();
     jsonData.push(newNote);
-    
+
     fs.writeFile('./db/db.json', JSON.stringify(jsonData), err => {
         if (err) {
             console.log('Error writing file', err);
@@ -38,6 +46,7 @@ app.post("/api/notes", (req, res) => {
     })
 });
 
+// This DELETE creates a new array and goes through all of the notes in the jsonData and adds them to the array so long as they do not have the id that matches the note to be deleted
 app.delete("/api/notes", (req, res) => {
     let newJson = [];
 
@@ -54,7 +63,7 @@ app.delete("/api/notes", (req, res) => {
             jsonData = newJson;
             return res.json(newJson);
         }
-    })
+    });
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
